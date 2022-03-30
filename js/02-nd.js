242214942dedb24b7c8faebbc312e6696f9ab644
neigiamasJonas <p.ezerskis@gmail.com>
2022-03-30 17:33:51 +0300

    Salyginiai uzdaviniai

---
+++ js/02-nd.js
@@ -0,0 +1,40 @@
+ // SALYGINIAI UZDAVINIAI (mini) //
+
+// 1 //
+
+const n = 20;
+console.log(n);
+
+if (n > 0) {
+    console.log('Geras');
+} else {
+    console.log('Blogas');
+}
+
+// 2 //
+
+const zalia = 1;
+const geltona = 2;
+const raudona = 3;
+
+const x = geltona;
+
+if (x == 2) {
+    console.log('Palaukite');
+}
+
+// 3 //
+
+const k = 1;        // knygos //
+const n2 = 5*k;     // kiek knygu telpa i deze //
+const d = n2;       // deze //
+
+if (2*d >= k*8) {
+    console.log('Knygos telpa i dezes')
+}
+if (3*d <= k*18) {
+    console.log('Knygos netelpa i dezes')
+}
+
+// 4 //
+
