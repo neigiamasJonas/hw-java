7334db55839a24f926d7e66610f92c10a088a4db
neigiamasJonas <p.ezerskis@gmail.com>
2022-04-07 15:00:53 +0300

    04-07 fix+ funkciju 8/9/10

--- js/06-nd-salyginiai-funkcijos.js
+++ js/06-nd-salyginiai-funkcijos.js
@@ -16,7 +16,7 @@ function lygina(a, b) {
     if (a < b) {
         return 'Antras skaicius didesnis us pirma';
     }
-    if (a = b) {
+    if (a === b) {
         return "Skaiciai yra lygus";
     }
 }
@@ -82,17 +82,31 @@ console.log('----------------');
 // 6 //
 
 function sveikas(a) {
-    let skaiciai = [];
+    let skaiciai = 0;
     for (i = 0; i < a; i++) {
         if (a % i === 0 && i !== 1) {
-            skaiciai.push(i);
+            skaiciai++
         }
     }
     return skaiciai;
 }
-console.log(sveikas(10));
+console.log(sveikas(20));
 
 console.log('----------------');
+
+
+// function sveikas(a) {
+//     let skaiciai = [];
+//     for (i = 0; i < a; i++) {
+//         if (a % i === 0 && i !== 1) {
+//             skaiciai.push(i);
+//         }
+//     }
+//     return skaiciai;
+// }
+// console.log(sveikas(20));
+
+// console.log('----------------');
 
 // 7 //
 
@@ -106,10 +120,65 @@ console.log('----------------');
 
 // 8 //
 
-function telNr(a) {
-    return [a];
+function telNr(num) {
+    if (num.length !== 10) {
+        return `Numeris turi tureti 10 skaitmenu`
+    } else {
+        return `Telefono numeris: (${num.slice(0, 3).join('')}) ${num.slice(3, 6).join('')}-${num.slice(6).join('')}`;
+    }
+    
 }
 
-console.log(telNr(1234567890));
+console.log(telNr([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
 
-// cia sustojau / nespejau dar / nebesigalvoja //
+// arba
+
+function telNr2(num) {
+    if (num.length !== 10) {
+        return `Numeris turi tureti 10 skaitmenu`
+    } else {
+        let numeris = '(xxx) xxx-xxxx';
+
+        num.forEach(element => {
+            numeris = numeris.replace('x', element);
+        
+    });
+    return `Telefono numeris2: ${numeris}`;   
+    }
+}
+
+console.log(telNr2([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
+
+console.log('----------------');
+
+// 9 //
+
+function text(a, b) {
+    if (a.length > b.length) {
+        return `Pirmas tekstas ilgesnis uz antra`;
+    } else if (b.length > a.length) {
+        return `Antras tekstas yra ilgesnis uz pirma`;
+    } else {
+        return `Tekstai yra lygus`;
+    }
+    
+}
+
+console.log(text('rope', 'salota'));
+
+console.log('----------------');
+
+// 10 //
+
+function raides(r) {
+    let skaicius = 0;
+
+    for (let i = 0; i < r.length; i++) {
+        if (r[i] === 'a') {
+            skaicius++
+        }
+    }
+    return `Zodyje yra ${skaicius} "a" raides`;
+}
+
+console.log(raides('Matematika'));
